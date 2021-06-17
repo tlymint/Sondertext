05538e26e3e0350f6e04244bdeb58de4cc4cac1d
JDO <jiayan.dong@bbr.net>
2021-06-17 11:27:27 +0200

    30%

--- src/app/dateneditor/dateneditor.component.ts
+++ src/app/dateneditor/dateneditor.component.ts
@@ -10,6 +10,9 @@ export class DateneditorComponent implements OnInit {
   
   @ViewChild('toolbar', {static: false}) toolbar:any
 
+  //variable using to resize container left and right
+  oldX = 0;
+  //
   hiddenTable: boolean = false;
   help_content:string = "This container used to help User to understand some difficulty operation.";
   constructor() { }
@@ -17,21 +20,29 @@ export class DateneditorComponent implements OnInit {
   ngOnInit() {
   }
 
-  leftContainerWidth: string = '20vw';
+  leftContainerWidth: number = 250;
   mouseDownOnHandle: boolean = false;
   highlightHandle: string = '#e3e3e6';
 
-  changeResizeMode(value: boolean): void{
+  changeResizeMode(value: boolean, event:MouseEvent): void{
     this.mouseDownOnHandle = value;
-    if(value===true)
+    if(value===true) {
       this.highlightHandle = '#e3e3e6';
+      this.oldX = event.clientX;
+    }
+      
     else
       this.highlightHandle = '#c9c9e6';
   }
   
   changeLeftContainerWidth(event: MouseEvent): void{
     if(this.mouseDownOnHandle)
-      this.leftContainerWidth = event.clientX - 10 + "px";
+    {
+      this.leftContainerWidth += (event.clientX - this.oldX);
+      this.oldX = event.clientX;
+    }
+      
+      
   }
 
 }
