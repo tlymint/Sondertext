6d7526a09425c438bd23534c03608a185356803b
JDO <jiayan.dong@bbr.net>
2021-06-21 20:56:25 +0200

    merge with table f Xiao a. new feat. highlig.

--- src/app/dateneditor/data-table/data-table.component.ts
+++ src/app/dateneditor/data-table/data-table.component.ts
@@ -4,9 +4,9 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { DataTableDataSource, DataTableItem } from './data-table-datasource';
-// Dong
+// Dong: right-click-contextmenu
 import { MatMenu, MatMenuTrigger } from '@angular/material';
-//
+// bis here
   
 @Component({
   selector: 'app-data-table',
@@ -15,24 +15,23 @@ import { MatMenu, MatMenuTrigger } from '@angular/material';
 })
 
 export class DataTableComponent implements AfterViewInit, OnInit {
-
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   @ViewChild(MatTable, {static: false}) table: MatTable<DataTableItem>;
+  @Input() toolbar: ToolbarComponent;
+  // Dong: right-click-contextmenu
   @ViewChild(MatMenuTrigger, {static: true})
   contextMenu: MatMenuTrigger;
   contextMenuPosition = { x: '0px', y: '0px' };
-  @Input() toolbar: ToolbarComponent;
-  
+  // bis here
 
   dataSource: DataTableDataSource;
-  
   grafikSource: any;
   clickedRows = new Set<DataTableItem>();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'Zeit', 'RZL', 'RZL_Button', 'Richtung', 'M_Zeit', 'M_Linie',
-                      'M_Kurs', 'Route', 'Fahrtart', 'Umlauf'];
+  'M_Kurs', 'Route', 'Fahrtart', 'Umlauf'];
 
   ngOnInit() {
     this.dataSource = new DataTableDataSource();
@@ -45,14 +44,18 @@ export class DataTableComponent implements AfterViewInit, OnInit {
     this.grafikSource = this.table.dataSource;
   }
 
-  
+  rowClicked:any;
+  changeTableRowColor(idx: any) { 
+  if(this.rowClicked === idx) this.rowClicked = -1;
+  else this.rowClicked = idx;
+  }
 
-  //
+  //Dong: right-click-contextmenu
   onContextMenu(event: MouseEvent, row: DataTableItem) {
     event.preventDefault();
  
     this.contextMenuPosition.x = event.pageX -250 + 'px';
-    this.contextMenuPosition.y = event.pageY -50 + 'px';
+    this.contextMenuPosition.y = event.pageY -140 + 'px';
     this.contextMenu.menuData = { 'item': row };
     this.contextMenu.menu.focusFirstItem('mouse');
     this.contextMenu.openMenu();
@@ -64,7 +67,13 @@ export class DataTableComponent implements AfterViewInit, OnInit {
   onContextMenuAction2() {
     alert(`Click on Action 2`);
   }
-  
+  // bis here
+  selectedRowIndex = -1;  
+
+  highlight(row){
+    this.selectedRowIndex = row.id;
+  }
 }
 
-
+//[ngClass]="{'highlight': selectedRowIndex == row.id}" (click)="highlight(row)"
+  
